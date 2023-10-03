b60ea190610ac6446a9d99519f7696a0fd5bbce3
vantyaevserega <vantyaevserega@yandex.ru>
2023-10-03 08:29:43 +0300

    post message

--- Source/WebViewSample/Views/MainView.axaml.cs
+++ Source/WebViewSample/Views/MainView.axaml.cs
@@ -3,26 +3,34 @@ using Avalonia.Controls;
 namespace WebViewSample.Views;
 public partial class MainView : UserControl
 {
+    private int cnt = 1;
+
     public MainView()
     {
         InitializeComponent();
         PART_Button.Click += PART_Button_Click;
-        PART_WebView.WebViewNewWindowRequested += PART_WebView_WebViewNewWindowRequested;
+        PART_WebView.NavigationCompleted += PART_WebView_NavigationCompleted;
+        PART_WebView.WebMessageReceived += PART_WebView_WebMessageReceived;
     }
 
-    private void PART_Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void PART_WebView_NavigationCompleted(object? sender, WebViewCore.Events.WebViewUrlLoadedEventArg e)
     {
-        Window window = new()
+        try
         {
-            Height = 600,
-            Width = 600
-        };
-
-        window.Show();
+            PART_WebView.PlatformWebView?.ExecuteScriptAsync("window.chrome.webview.addEventListener('message', function(e) { alert('Clicks: ' + e.data); });");
+            PART_WebView.PlatformWebView?.ExecuteScriptAsync("window.chrome.webview.postMessage(\"Click me\");");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
     }
 
-    private void PART_WebView_WebViewNewWindowRequested(object? sender, WebViewCore.Events.WebViewNewWindowEventArgs e)
+    private void PART_WebView_WebMessageReceived(object? sender, WebViewCore.Events.WebViewMessageReceivedEventArgs e)
     {
-        e.UrlLoadingStrategy = WebViewCore.Enums.UrlRequestStrategy.OpenInNewWindow;
+        PART_Button.Content = e.Message;
+        PART_Button.IsEnabled = true;
     }
+
+    private void PART_Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e) => PART_WebView.PostWebMessageAsString((++cnt).ToString(), PART_WebView.Url);
 }
